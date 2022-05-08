97bac47124481d8aae1f4e94cc55b02b0bcb3bc1
Marius <mariusthiesset@gmail.com>
2022-05-08 22:02:34 +0200

    homepage + fomulaire Formik

--- src/components/LoginForm/Login.js
+++ src/components/LoginForm/Login.js
@@ -1,24 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from "react";
 import axios from "axios";
 import jwt_decode from "jwt-decode";
 import Api from "../Api"
 const baseURL = "api/auth-token";
 
 export default function Login() {
-    const [datas, setDatas] = useState({
-        uuid: null,
-        password: null
-    });
-
-
-    const handleSubmit = e => {
-        e.preventDefault();
-
-        setDatas({
-            uuid: e.target.uuid.value,
-            password: e.target.password.value
-        })
-
+    const handleSubmit = (values) => {
         axios
             .post(baseURL, datas)
             .then((response) => {
@@ -28,35 +15,37 @@ export default function Login() {
                 console.log(err);
             });
     }
-    
-    if (localStorage.getItem('token')) {
-        const token = localStorage.getItem('token');
-        const decoded = jwt_decode(token);
-
-        if (decoded.exp < Date.now() / 1000) {
-            localStorage.clear();
-        }
-        
-        Api.put('scopes/1', {
-            accreditation: ['/api/accreditations/186', '/api/accreditations/177'],
-        });
-    }
-
-
 
     return (
     <>
-        <div>
-        <h1>Connection</h1>
-        <form onSubmit={handleSubmit}>
-            <label>Uuid</label>
-            <input type='number' name='uuid'></input>
+        <Container>
+            <h1 className="d-flex justify-content-center mb-5">Bienvenue sur Modulo !</h1>
+            <Formik
+                initialValues={{ uuid: "", password: "" }}
+                onSubmit={handleSubmit}
+                validate={validate}
+            >
+                {({ isSubmitting }) => (
+                    <Form className="col g-1">
+                        <div className="col-6">
+                            <label htmlFor="firstName">N°adhérent</label>
+                            <Field type="number" name="uuid" placeholder="N°adhérent" className="form-control" />
+                            <ErrorMessage name="uuid" component="div" className="error-form" />
+                        </div>
 
-            <label>Password</label>
-            <input type='text' name='password'></input>
-            <button>Connexion</button>
-        </form>
-        </div>
+                        <div className="col-6">
+                            <label htmlFor="firstName">Mot de passe</label>
+                            <Field type="password" name="password" placeholder="Mot de passe" className="form-control" />
+                            <ErrorMessage name="password" component="div" className="error-form" />
+                        </div>
+
+                        <button type="submit" disabled={isSubmitting} className="btn btn-light col-auto my-3">
+                            Envoyer
+                        </button>
+                    </Form>
+                )}
+            </Formik>
+        </Container>
     </>
     )
 }
