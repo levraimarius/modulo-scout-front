478845cfc27d269870bf130c72a8147d54e43e60
Marius <mariusthiesset@gmail.com>
2022-06-06 23:01:52 +0200

    Liste event + ajout event

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.scss';
 import { BrowserRouter, Routes, Route, Navigate, NavLink } from "react-router-dom";
 import Login from './components/LoginForm/Login';
 import FunctionAccreditation from './components/FunctionAccreditation/FunctionAccreditation';
+import ListEvents from './components/Events/ListEvents';
+import AddEvents from './components/Events/AddEvents';
+import EditEvents from './components/Events/EditEvents';
 import RolesList from './components/RolesList/RoleList';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CategoryList from './components/Category/CategoryList';
@@ -51,7 +54,7 @@ function App() {
 
   return (
     <>
-      <Navbar colapseonselect bg="light" expand="sm" sticky='top' className="border-bottom border-purple mb-5">
+      <Navbar colapseonselect="true" bg="light" expand="sm" sticky='top' className="border-bottom border-purple mb-5">
         <Container className='m-0'>
           <Navbar.Brand href="/">Modulo</Navbar.Brand>
           <Navbar.Toggle aria-controls='responsive-navbar-nav' />
@@ -65,6 +68,7 @@ function App() {
                   {
                     isAdmin() &&
                       <NavDropdown title="Backoffice" id="basic-nav-dropdown">
+                      <NavDropdown.Item href='/event-list'>Liste des événements</NavDropdown.Item>
                         <NavDropdown.Item href='/roles'>Roles</NavDropdown.Item>
                         <NavDropdown.Item href='/event-categories'>Catégories d'événements</NavDropdown.Item>
                         <NavDropdown.Item href='/users/1'>Liste utilisateurs</NavDropdown.Item>
@@ -85,6 +89,9 @@ function App() {
           { (localStorage.getItem("token") === null && localStorage.getItem("currentScope")) === null && <Route path="/" element={<Login />} /> }
           { (localStorage.getItem("token") && localStorage.getItem("currentScope") !== null) && <Route path="/" element={<ConnectedHomepage />} /> }
           { (localStorage.getItem("token") && localStorage.getItem("currentScope") === null) && <Route path="/" element={<ScopeChoice user={user} />} /> }
+          <Route path="/event-list" element={isAdmin() ? <ListEvents /> : <Navigate to="/" />}/>
+          <Route path="/event-list/add"  element={isAdmin() ? <AddEvents /> : <Navigate to="/" />} />
+          <Route path="/event-list/edit/:id" element={isAdmin() ? <EditEvents /> : <Navigate to="/" />} />
           <Route path="/roles" element={isAdmin() ? <RolesList /> : <Navigate to="/" />}/>
           <Route path="/roles/accreditation/:id" element={isAdmin() ? <FunctionAccreditation /> : <Navigate to="/" />} />
           <Route path="/event-categories" element={isAdmin() ? <CategoryList /> : <Navigate to="/" />} />
---
+++ src/components/Events/ListEvents.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import Api from "../Api";
+import {
+    BrowserRouter as Router,
+    Link
+} from "react-router-dom";
+import { Icon } from '@iconify/react';
+
+const baseURL = "api/auth-token";
+const token = localStorage.getItem('token');
+
+export default function ListEvents() {
+    const [events, setEvents] = useState(null);
+    
+    useEffect(() => {
+        Api.get('/events')
+        .then((response) => {
+            setEvents(response.data);
+        })
+    }, []);
+
+    const deleteItem = (id) => {
+        Api.delete(`/events/${id}`)
+        .then(res => {
+            window.location.href = "/event-list"
+        });
+    }
+    
+    return (
+    <>
+        <div className="container mt-5">
+            <h1>Liste des événements</h1>
+            <Link
+                to={{
+                    pathname: "/event-list/add",
+                }}
+            ><button type="button" className="btn btn-success mx-auto">Ajouter un événement</button></Link>
+            <div>
+                {!events && "Chargement en cours."}
+                <table className="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">Titre</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Dates</th>
+                        <th scope="col">Interactions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {events && events.map(event => (
+                            <tr key={event.id}>
+                                <th scope="row">{event.id}</th>
+                                <td>{event.title}</td>
+                                <td><div
+                                        dangerouslySetInnerHTML={{__html: event.description}}
+                                    />
+                                </td>
+                                <td>{event.start_at} - {event.end_at}</td>
+                                <td>
+                                    <Link to={`/event-list/edit/${event.id}`} className="btn btn-success"><Icon icon="bxs:edit"/></Link>
+                                    <button type="button" className="btn btn-danger" onClick={() => deleteItem(event.id)}><Icon icon="fluent:delete-24-filled"/></button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </>
+    )
+}
